236accb29f7fdd6c7af44d022a8f667d4b9d7200
Verseler <verselerkerr.handuman@gmail.com>
2025-03-03 10:18:49 +0800

    style: add border error indicator when input value is invalid

--- resources/js/Components/ui/input/Input.vue
+++ resources/js/Components/ui/input/Input.vue
@@ -7,6 +7,7 @@ const props = defineProps<{
     defaultValue?: string | number;
     modelValue?: string | number;
     class?: HTMLAttributes["class"];
+    invalid?: boolean;
 }>();
 
 const emits = defineEmits<{
@@ -25,7 +26,8 @@ const modelValue = useVModel(props, "modelValue", emits, {
         :class="
             cn(
                 'flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-primary-400 dark:placeholder:text-neutral-400 dark:focus-visible:ring-primary-300',
-                props.class
+                props.class,
+                invalid && 'border-red-500 focus:border-red-500 '
             )
         "
     />
