77135b8ad7ff5ea8c1be7d9683e92e868651dd26
Verseler <vrslr2003@gmail.com>
2025-03-08 15:56:35 +0800

    fix: tooltip provider not found by adding in the app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { TooltipProvider } from '@/Components/ui/tooltip';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,7 +17,11 @@ createInertiaApp({
             import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        createApp({ 
+            render: () => h(TooltipProvider, { delayDuration: 300 }, [
+                h(App, props)
+            ]) 
+        })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
