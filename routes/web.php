4de159702e3627885d997e6e4e84901ab6552619
Verseler <vrslr2003@gmail.com>
2025-03-08 15:57:18 +0800

    move room routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,42 @@
 <?php
 
-use App\Models\Office;
-use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\RoomController;
 use App\Http\Controllers\ReservationSubmissionController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 //* Guests
 Route::get('/', function () {
-    $offices = Office::all();
-    $host_office = Office::findOrFail(1); //TODO: make this dyamic
-
-    return Inertia::render('LandingPage', [
-        'canLogin' => Route::has('login'),
-        'offices' => $offices,
-        'hostOffice' => $host_office
-    ]);
+return Inertia::render('LandingPage');
 });
 
-//! temporay
-Route::post('/reservation', [ReservationSubmissionController::class, 'create'])->name('reservation.create');
+//* Guest Reservation
+Route::middleware('guest')->group(function () {
+    Route::get('/reservation', [ReservationSubmissionController::class, 'form'])->name('reservation.form');
+    Route::post('/reservation', [ReservationSubmissionController::class, 'create'])->name('reservation.create');
+    Route::get('/reservation/confirmation', [ReservationSubmissionController::class, 'confirmation'])->name('reservation.confirmation');
+});
+
+//* Rooms
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/rooms', [RoomController::class, 'list'])->name('room.list');
+    Route::inertia('/rooms/create', 'RoomManagement/CreateRoom')->name('room.createForm');
+    Route::post('/rooms/create', [RoomController::class, 'create'])->name('room.create');
+    Route::get('/rooms/edit/{id}', [RoomController::class, 'editForm'])->name('room.editForm');
+    Route::put('/rooms/edit/{room}', [RoomController::class, 'edit'])->name('room.edit');
+    Route::get('/rooms/{id}', [RoomController::class, 'show'])->name('room.show');
+    Route::delete('/rooms/{id}', [RoomController::class, 'delete'])->name('room.delete');
+});
+
+
+//* Offices
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/offices', [OfficeController::class, 'list'])->name('office.list');
+    Route::get('/offices/form/{id?}', [OfficeController::class, 'upsertForm'])->name('office.upsertForm');
+    Route::post('/offices/upsert/{id?}', [OfficeController::class, 'upsert'])->name('office.upsert');
+    Route::delete('/offices/{id}', [OfficeController::class, 'delete'])->name('office.delete');
+});
 
 
 //* Admins
@@ -28,6 +44,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
 });
 
-
-require __DIR__ . '/room.php';
 require __DIR__ . '/auth.php';
