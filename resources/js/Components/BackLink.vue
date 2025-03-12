4b5f7e10a1c77f558f071a3b8166ffe558743f42
verseler <handuman.verselerkerr@gmail.com>
2025-03-12 13:21:32 +0800

    add href props to allows a custom routing path

--- resources/js/Components/BackLink.vue
+++ resources/js/Components/BackLink.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import Button from "@/Components/ui/button/Button.vue";
 import { ArrowLeft } from "lucide-vue-next";
+import { Link } from "@inertiajs/vue3";
+
+type BackLinkProps = {
+    href?: string;
+}
+
+const { href } = defineProps<BackLinkProps>();
+
 </script>
 
 <template>
-    <a onclick="history.back();return false;">
+    <Link v-if="href" :href="href">
+        <Button
+            variant="outline"
+            size="icon"
+            class="border-primary-500 text-primary-500 hover:bg-primary-50 hover:text-primary-600"
+        >
+            <ArrowLeft />
+        </Button>   
+    </Link>
+    
+    <a v-else onclick="history.back();return false;">
         <Button
             variant="outline"
             size="icon"
@@ -14,3 +32,4 @@ import { ArrowLeft } from "lucide-vue-next";
         </Button>   
     </a>
 </template>
+
